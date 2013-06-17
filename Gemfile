30e9968b9f9feb8c33302217e66b9602d6e651c2
Brian <babsetz@barquin.com>
2013-06-17 16:59:07 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.12'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+gem 'rspec-rails', '~> 2.12'
+gem 'sauce', '~> 3.0.2'
+gem 'sauce-connect'
+gem 'capybara', '~> 2.0.3'
+gem 'parallel_tests'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sauce::Application.config.secret_token = '788e54ef2d2e4425d3d219b31497efed767487e0307f344e2fd9ff58adaec1ff333ebf45d68fff74ef283c52a341de2dc5469450c6bd31b5deb2baeed9bac248'
