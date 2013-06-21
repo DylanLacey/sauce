c93c7e8db14d52f9b19a49d4a6d88a829a7b108a
Dylan Lacey <github@dylanlacey.com>
2013-06-21 07:41:56 -0700

    Added extra logging

--- spec/features/miso_spec.rb
+++ spec/features/miso_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 describe "Wikipedia's Miso Page", :sauce => true do
   it "Should mention a favorite type of Miso" do
     visit "http://en.wikipedia.org/"
+    STDERR.puts "##  Visited Wikipedia"
     fill_in 'search', :with => "Miso"
+    STDERR.puts "##  Filled in form"
     click_button "searchButton"
+    STDERR.puts "##  Clicked button"
     page.should have_content "Akamiso"
+    STDERR.puts "## Page had content"
   end
 end
